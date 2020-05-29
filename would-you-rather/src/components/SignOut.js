37cca70c4a35457415cfebeafc2517aa65060ba8
Blade Dyer <blade.dyer@gmail.com>
2020-05-28 23:34:12 -0700

    added styles for navigation

--- would-you-rather/src/components/SignOut.js
+++ would-you-rather/src/components/SignOut.js
@@ -4,6 +4,22 @@ import Avatar from '@material-ui/core/Avatar'
 import Button from '@material-ui/core/Button'
 import {setAuthedUser} from '../actions/authedUser'
 
+const styles = {
+    signOutBtn:{
+        margin: 10
+    },
+    container:{
+        float: "right",
+        display: "flex"
+    },
+    name: {
+        marginRight: 10
+    },
+    avatar:{
+        alignSelf: "center"
+    }
+}
+
 
 class SignOut extends Component{
     handleSignOut =(e) =>{
@@ -15,10 +31,10 @@ class SignOut extends Component{
     render(){
         const {user} = this.props
         return(
-            <div>
-                <p>Hello, {user.name}</p>
-                <Avatar alt={user.name} src={user.avatarURL}/>
-                <Button variant="contained" color="secondary" onClick={this.handleSignOut}>Sign out</Button>
+            <div style={styles.container}>
+                <p style={styles.name}>Hello, {user.name}</p>
+                <Avatar style={styles.avatar} alt={user.name} src={user.avatarURL}/>
+                <Button style={styles.signOutBtn} variant="contained" color="secondary" onClick={this.handleSignOut}>Sign out</Button>
             </div>
         )
     }
